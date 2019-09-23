a9d67c91ab1d3520a2f338596d62a5070eaf4f1c
184805063 <55072405+184805063@users.noreply.github.com>
2019-09-23 22:13:44 +0800

    Add files via upload

---
+++ Example134.java
@@ -0,0 +1,11 @@
+class Aniaml{
+	void shout(){
+		System.out.println("¶¯Îï½Ð!");
+	}
+}
+public class Example134{
+	public static void main(String[] args){
+	   Animal animal=new Animal();
+	   System.out.println(animal.toString());
+	}
+}
