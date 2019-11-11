2314413aac62e8252d8625cd1438200dfce82c81
184805063 <55072405+184805063@users.noreply.github.com>
2019-11-11 23:47:30 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,39 @@
-import javax.xml.parsers.*;
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-import java,io.*;
-public class XMLUtil{
-public static Object getBean(){
-try{
-DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
-DocumentBuilder builder = dFactory.newDocumentBuilder();
-Document doc = builder.parse(new File("conflg.xml"));
-NodeList nl = doc.getElementsByTagName("className");
-Node classNode = nl.item(0).getFirstChild();
-String cName = classNode.getNodeValue();
-Class c = Class.forName(cName);
-Object obj = c.newInstance();
-return obj;
+package zuoye2;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	static int count;
+	static ArrayList list=new ArrayList();
+	public static void main(String[] args) throws InterruptedException 
+	 {
+		
+		Student stu=new Student();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("欢迎使用学生管理系统!");
+		Thread.sleep(200);
+		while(true) {
+		System.out.println("1-添加学生");
+		System.out.println("2-删除学生");
+		System.out.println("3-修改信息");
+		System.out.println("4-查询学生");
+		System.out.println("5-退      出");
+		
+		System.out.println("请输入你的选择：");
+		int choice=sc.nextInt();
+		if(choice==5)
+			break;
+		else {
+			switch(choice) {
+			case 1:Add.add();break;
+			case 2:Delete.delete();break;
+			case 3:Change.change();break;
+			case 4:Find.find();break;
+			}
+			
+		}
+		
+		}
+	}
+
 }
-catch(Exception e){
-e.printStackTrace();
-return null;
-}}
-public class Main{
-public static void main(Stirng[] args){
-TV tv;
-Factory factory;
-factory = (TVFactory)XMLUtil.getBean();
-tv = factory.produceTV();
-tv.play();
-}}
