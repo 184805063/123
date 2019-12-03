08c8455a3edeb3e336c9948633a715c7b188b495
184805063 <55072405+184805063@users.noreply.github.com>
2019-12-03 22:25:23 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-import tv.*;
-import tvfactory.*;
-import xml.ReadXMLUtilDX;
+import mv.*;
+import ct.*;
+import rt.*;
 public class Main{
-public static void main(String args[]){
-Dx dx;
-String brandName =ReadXMLUtilDX.getBrandName();
-dx =DXFactory.produceDx(brandName);
-dx.play();
-}
+  public static void main(String[] args) {
+      Customer c=new Customer("张三");
+      c. addRental(new Rental(new Movie("栀子花开",2),9));
+      c. addRental(new Rental(new Movie("山楂树之恋" ,2),8));
+      c. addRental(new Rental(new Movie("战狼",1),7));
+      System. out.print(c. statement());
+  }
 }
