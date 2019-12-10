f2a192e38ef38304b126bd229f47b1429c99e254
184805063 <55072405+184805063@users.noreply.github.com>
2019-12-10 22:04:14 +0800

    Add files via upload

---
+++ DBUtil.java
@@ -0,0 +1,57 @@
+package cn.itcast.application;
+
+import java.util.HashMap;
+
+public class DBUtil {
+	private static DBUtil instance = null;
+	private HashMap<String,User> users = new HashMap<String,User>();
+	
+	private DBUtil(){
+		
+		
+		User u1=new User();
+		u1.setCardId("1001");
+		u1.setCardPwd("123456");
+		u1.setUserName("刘备");
+		u1.setCall("15678230984");
+		u1.setAccount(10000);
+		users.put(u1.getCardId(), u1);
+		
+		User u2=new User();
+		u2.setCardId("1002");
+		u2.setCardPwd("000000");
+		u2.setUserName("关羽");
+		u2.setCall("16738329473");
+		u2.setAccount(20000);
+		users.put(u2.getCardId(), u2);
+		
+		User u3=new User();
+		u3.setCardId("1003");
+		u3.setCardPwd("654321");
+		u3.setUserName("张飞");
+		u3.setCall("17836936748");
+		u3.setAccount(30000);
+		users.put(u3.getCardId(), u3);
+	}
+	//懒汉式单例模式
+	public static DBUtil getInstance(){
+		if(instance == null){
+			synchronized(DBUtil.class){
+				if(instance == null){
+					instance = new DBUtil();
+				}
+			}
+		}
+		return instance;
+	}
+	//根据银行卡号获取对应单个账户的信息
+	public User getUser(String cardId){
+		User user = (User) users.get(cardId);
+		return user;
+	}
+	//获取所有账户的信息
+	public HashMap<String, User> getUsers(){
+		return users;
+	}
+
+}
